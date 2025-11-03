c19449155cf304b41dd66467771952eac2f62a44
Daniel-Prada27 <danielspradah0@gmail.com>
2025-11-03 15:56:20 -0500

    Add logic to create order

--- app/services.py
+++ app/services.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from database import load_items, load_orders, save_items, save_orders
 
-from models import Item
+from models import Item, OrderItem, Order
 
 
 def create_item(item):
@@ -17,3 +17,35 @@ def create_item(item):
     save_items(df)
     return item.sku
 
+def create_order(order):
+    df_items = load_items()
+    df_orders = load_orders()
+
+    for item in order.items:
+        if (item.sku not in df_items['sku'].values):
+            raise ValueError(f"Product sku={item.sku} not found")
+        
+        stock = df_items.loc[df_items['sku'] == item.sku, 'stock'].squeeze()
+        if (stock < item.qty):
+            raise ValueError(f"Insufficient stock for product sku={item.sku}")
+        
+    total_cost = 0
+    for item in order.items:
+        data_idx = df_items.index[df_items["sku"] == item.sku][0]
+        
+        cost = df_items.loc[data_idx, 'unit_cost']
+
+        total_cost += cost * item.qty
+
+        df_items.loc[data_idx, 'stock'] -= item.qty
+
+        new_order = {'order_id': order.order_id, "sku": item.sku, "qty": item.qty}
+
+        new_order = pd.DataFrame([new_order])
+
+        df_orders = pd.concat([df_orders, new_order], ignore_index=True)
+
+    save_items(df_items)
+    save_orders(df_orders)
+
+    return {"order_id": order.order_id, "status": "success", "total_cost": float(total_cost)}
